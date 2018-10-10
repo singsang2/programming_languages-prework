3f3fe90487338d9284836439ff25bf207348227f
Sung Bae <singsang2@gmail.com>
2018-10-10 16:29:59 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,24 @@
 def reformat_languages(languages)
   # your code here
+  reformat = {}
+  get_names(languages).each do |language|
+    reformat[language] = {:type => get_type(languages, language)[0], :style => get_style(languages, language)}
+  end
+  reformat
+end
+
+def get_names(languages)
+  (languages.collect {|style, data| data.keys}).flatten.uniq
+end
+
+def get_type(languages, language)
+  type = []
+  languages.each {|style, data| type.push(data.fetch(language, nil).fetch(:type)) if data.fetch(language, nil) != nil}
+  type.uniq
+end
+
+def get_style(languages, language)
+  style_list = []
+  languages.each {|style, data| style_list.push(style) if data.keys.include?(language)}
+  style_list.uniq
 end
